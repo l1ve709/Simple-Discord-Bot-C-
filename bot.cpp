85edfd6744be5d6223c440560c101dc79b2b402a
l1ve709 <nof4ce1337@gmail.com>
2024-09-30 20:15:10 +0300

    bot.cpp

--- bot.cpp
+++ bot.cpp
@@ -5,5 +5,16 @@
 void Bot::start() {
     std::cout << "Bot is starting." << std::endl;
     CommandManager commandManager;
-
+    
+    commandManager.addCommand("/ban", "Ban a user from the server");
+    commandManager.addCommand("/kick", "Kick a user from the server");
+    commandManager.addCommand("/warn", "Warn a user for a specific reason");
+    commandManager.addCommand("/joke", "Tell a joke");
+    commandManager.addCommand("/trivia", "Ask a trivia question");
+    
+    commandManager.executeCommand("/ban user1");
+    commandManager.executeCommand("/kick user2");
+    commandManager.executeCommand("/warn user3 spam");
+    commandManager.executeCommand("/joke");
+    commandManager.executeCommand("/trivia");
 }
