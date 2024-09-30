80cba63be7cab3295be8e9e794f37d0b069a3c01
l1ve709 <nof4ce1337@gmail.com>
2024-09-30 20:08:52 +0300

    bot.cpp

---
+++ bot.cpp
@@ -0,0 +1,9 @@
+#include "bot.h"
+#include <iostream>
+#include "commands/command_manager.h"
+
+void Bot::start() {
+    std::cout << "Bot is starting." << std::endl;
+    CommandManager commandManager;
+
+}
