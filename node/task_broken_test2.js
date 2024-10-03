741066d6c3288477bfe86340bcb06645d1ecd190
Ediz <nof4ce1337@gmail.com>
2024-10-04 01:35:47 +0300

    task_broken_test2.js

---
+++ node/task_broken_test2.js
@@ -0,0 +1,44 @@
+function executeTaskM() {
+    console.log("task M runninng");
+}
+executeTaskM();
+
+function executeTaskM() {
+    console.log("task M runninng");
+}
+executeTaskM();
+
+function executeTaskM() {
+    console.log("task M runninng");
+}
+executeTaskM();
+
+function executeTaskM() {
+    console.log("task M runninng");
+}
+executeTaskM();
+
+function executeTaskM() {
+    console.log("task M runninng");
+}
+executeTaskM();
+
+function executeTaskM() {
+    console.log("task M runninng");
+}
+executeTaskM();
+
+function executeTaskM() {
+    console.log("task M runninng");
+}
+executeTaskM();
+
+function executeTaskM() {
+    console.log("task M runninng");
+}
+executeTaskM();
+
+function executeTaskM() {
+    console.log("task M runninng");
+}
+executeTaskM();
