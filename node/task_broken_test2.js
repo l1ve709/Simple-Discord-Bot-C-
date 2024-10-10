1bfc1baba4ef1aa8021aaa21cbddc17cd474ab89
Ediz <nof4ce1337@gmail.com>
2024-10-10 17:30:05 +0300

    loop

--- node/task_broken_test2.js
+++ node/task_broken_test2.js
@@ -14,7 +14,87 @@ executeTaskM();
 function executeTaskM() {
     console.log("task M runninng");
 }
+executeTaskM();function executeTaskM() {
+    console.log("task M runninng");
+}
 executeTaskM();
+
+function executeTaskM() {
+    console.log("task M runninng");
+}
+executeTaskM();function executeTaskM() {
+    console.log("task M runninng");
+}
+executeTaskM();
+
+function executeTaskM() {
+    console.log("task M runninng");
+}
+executeTaskM();function executeTaskM() {
+    console.log("task M runninng");
+}
+executeTaskM();
+
+function executeTaskM() {
+    console.log("task M runninng");
+}
+executeTaskM();function executeTaskM() {
+    console.log("task M runninng");
+}
+executeTaskM();
+
+function executeTaskM() {
+    console.log("task M runninng");
+}
+executeTaskM();function executeTaskM() {
+    console.log("task M runninng");
+}
+executeTaskM();
+
+function executeTaskM() {
+    console.log("task M runninng");
+}
+executeTaskM();function executeTaskM() {
+    console.log("task M runninng");
+}
+executeTaskM();
+
+function executeTaskM() {
+    console.log("task M runninng");
+}
+executeTaskM();function executeTaskM() {
+    console.log("task M runninng");
+}
+executeTaskM();
+
+function executeTaskM() {
+    console.log("task M runninng");
+}
+executeTaskM();function executeTaskM() {
+    console.log("task M runninng");
+}
+executeTaskM();
+
+function executeTaskM() {
+    console.log("task M runninng");
+}
+executeTaskM();function executeTaskM() {
+    console.log("task M runninng");
+}
+executeTaskM();
+
+function executeTaskM() {
+    console.log("task M runninng");
+}
+executeTaskM();function executeTaskM() {
+    console.log("task M runninng");
+}
+executeTaskM();
+
+function executeTaskM() {
+    console.log("task M runninng");
+}
+executeTaskM();v
 
 function executeTaskM() {
     console.log("task M runninng");
